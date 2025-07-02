6147ccb61ddd72136f688ffea472afbe00e9d0b6
Sylvain Fraresso <sylvainfraresso@laposte.net>
2025-07-02 14:58:26 +0200

    problem17

--- src/1-99/problem17.py
+++ src/1-99/problem17.py
@@ -1,52 +1,24 @@
-number_letter_dict = {
-1: 3,
-2: 3,
-3: 5,
-4: 4,
-5: 4,
-6: 3,
-7: 5,
-8: 5,
-9: 4,
-10: 3,
-11: 6,
-12: 6,
-13: 8,
-14: 8,
-15: 7,
-16: 7,
-17: 9,
-18: 9,
-19: 8,
-20: 6,
-30: 6,
-40: 5,
-50: 5,
-60: 5,
-70: 7,
-80: 7,
-90: 6,
+letter_count_dict = {
+    0:0, 1:3, 2:3, 3:5, 4:4, 5:4, 6:3, 7:5, 8:5, 9:4,
+    10:3, 11:6, 12:6, 13:8, 14:8, 15:7, 16:7, 17:9, 18:8, 19:8,
+    20:6, 30:6, 40:5, 50:5, 60:5, 70:7, 80:6, 90:6, 
+    100:10, 200:10, 300:12, 400:11, 500:11, 600:10, 700:12, 800:12, 900:11,
 }
 
-def number_letter_count(n):
-    if n == 0:
-        return 0
-    if n < 20:
-        return number_letter_dict[n]
-    if n < 100:
-        return number_letter_dict[n // 10 * 10] + number_letter_count(n % 10)
-    if n == 1000:
-        return 11
-    if n % 100 != 0:
-        return number_letter_dict[n // 100] + 7 + 3 + number_letter_count(n % 100)
+def count_letters(number):
+    if number <= 20:
+        return letter_count_dict[number]
+    elif number < 100:
+        return letter_count_dict[(number // 10) * 10] + count_letters(number % 10)
+    elif number < 1000:
+        if number % 100 == 0:
+            return letter_count_dict[(number // 100) * 100]
+        else:
+            return letter_count_dict[(number // 100) * 100] + count_letters(number % 100) + 3 # and
     else:
-        return number_letter_dict[n // 100] + 7
+        return 11 # 1000
 
 
-def number_letter_up_to(n):
-    nb_letter = 0
-    for i in range(1, n + 1):
-        nb_letter += number_letter_count(i)
-    return nb_letter
-
-print(number_letter_up_to(1000))
+print(count_letters(342))
+print(count_letters(115))
+print(sum(count_letters(n) for n in range(1, 1000 + 1)))
