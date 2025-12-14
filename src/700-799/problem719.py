0679961fcad77e79ff00c3d552f8470180c50dfe
unknown <sylvainfraresso@laposte.net>
2025-12-14 14:34:47 +0100

    problem97

--- src/700-799/problem719.py
+++ src/700-799/problem719.py
@@ -1,40 +1,32 @@
-from math import sqrt
+from itertools import combinations
 
 
-def generate_split_sums(n, nb_split, memo=dict()):
-    if (tuple(n), nb_split) in memo:
-        return memo[(tuple(n), nb_split)]
-    if nb_split == 0:
-        return [int(''.join(n))]
-    sums = []
-    for end_idx in range(len(n) - nb_split):
-        current_term = int(''.join(n[:end_idx + 1]))
-        for subsum in generate_split_sums(n[end_idx + 1:], nb_split - 1):
-            sums.append(current_term + subsum)
-    memo[(tuple(n), nb_split)] = sums
-    return sums
+def is_s_number(n):
+    root = int(n**(1/2))
+    digit_list = list(str(n))
+    nb_digits = len(digit_list)
 
-def test_s_number(i, n):
-    for nb_split in range(1, len(n)):
-        for split_sum in generate_split_sums(n, nb_split):
-            if split_sum == i:
+    for nb_split in range(nb_digits - 1, 0, -1):
+        max_possible_sum = 10**(nb_digits - nb_split) - 1 + 9 * nb_split
+        if max_possible_sum < root:
+            continue
+
+        for split in combinations(range(1, nb_digits), nb_split):
+            indexes = (0,) + split + (nb_digits,)
+            if sum(int(''.join(digit_list[indexes[i]:indexes[i + 1]])) for i in range(len(indexes) - 1)) == root:
                 return True
+        
     return False
+
 
 def T(N):
     res = 0
-    for i in range(1, int(sqrt(N) + 1)):
-        if i % 1000 == 0:
-            print(i)    
-        n = list(str(i**2))
-        if test_s_number(i, n):
-            res += i**2
+    for i in range(1, int(N**(1/2) + 1)): 
+        n = i**2
+        if is_s_number(n):
+            res += n
     return res
 
 
-# print(test_s_number(9, ['8','1']))
-# print(test_s_number(82, ['6','7','2','4']))
-# print(test_s_number(91, ['8','2','8','1']))
-# print(test_s_number(99, ['9','8','0','1']))
-
+print(T(10**4))
 print(T(10**12))
