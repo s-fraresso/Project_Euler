648a2dc8367d88ec674123a59237b5a9ea6257f6
unknown <sylvainfraresso@laposte.net>
2026-01-13 23:18:28 +0100

    problem-1.py

---
+++ src/bonus/minus1.py
@@ -0,0 +1,5 @@
+"""
+On somme les multiples de 3 et de 5 et on soustrait les multiples de 15
+ce qui donne, en notant S = sum_{n=1}^{+\inf}, 3S + 5S - 15S.
+On utilise alors l'identité S = zeta(-1) = -1/12 ce qui donne 7/12.
+"""
